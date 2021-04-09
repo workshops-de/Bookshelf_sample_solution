19b8c9c751d357adf626799d2430c85d6761da3e
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-04-09 13:22:42 +0200

    Solution for task: "Test with MvcResult"

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerIntegrationTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerIntegrationTest.java
@@ -1,16 +1,19 @@
 package de.workshops.bookshelf.book;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,12 +22,21 @@ class BookRestControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     void getAllBooks() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(BookRestController.REQUEST_URL))
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(BookRestController.REQUEST_URL))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].title", is("Clean Code")));
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].title", is("Clean Code")))
+                .andReturn();
+        String jsonPayload = mvcResult.getResponse().getContentAsString();
+
+        Book[] books = objectMapper.readValue(jsonPayload, Book[].class);
+        assertEquals(3, books.length);
+        assertEquals("Clean Code", books[1].getTitle());
     }
 }
