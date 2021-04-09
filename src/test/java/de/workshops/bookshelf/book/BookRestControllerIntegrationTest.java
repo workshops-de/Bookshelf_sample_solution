09eaf924a8a472d3c3a37eb181745964f15fdbbb
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-04-09 13:56:23 +0200

    Solution for task: "Write REST Assured test for BookRestController"

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerIntegrationTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerIntegrationTest.java
@@ -1,6 +1,8 @@
 package de.workshops.bookshelf.book;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,13 +13,15 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureMockMvc
 class BookRestControllerIntegrationTest {
+
+    @Autowired
+    private BookRestController bookRestController;
 
     @Autowired
     private MockMvc mockMvc;
@@ -38,5 +42,32 @@ class BookRestControllerIntegrationTest {
         Book[] books = objectMapper.readValue(jsonPayload, Book[].class);
         assertEquals(3, books.length);
         assertEquals("Clean Code", books[1].getTitle());
+    }
+
+    @Test
+    void testWithRestAssuredMockMvc() {
+        RestAssuredMockMvc.standaloneSetup(bookRestController);
+        RestAssuredMockMvc.
+                given().
+                log().all().
+                when().
+                get("/book").
+                then().
+                log().all().
+                statusCode(200).
+                body("author[0]", equalTo("Erich Gamma"));
+    }
+
+    @Test
+    void testWithRestAssured() {
+        RestAssured.
+                given().
+                log().all().
+                when().
+                get("/book").
+                then().
+                log().all().
+                statusCode(200).
+                body("author[0]", equalTo("Erich Gamma"));
     }
 }
