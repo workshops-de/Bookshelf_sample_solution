02608606c79c53549d386bad07ce15f869ee131b
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-04-10 17:51:50 +0200

    Solution for task: "Add a TestConfiguration"

--- src/test/java/de/workshops/bookshelf/EnvironmentTest.java
+++ src/test/java/de/workshops/bookshelf/EnvironmentTest.java
@@ -1,29 +1,21 @@
 package de.workshops.bookshelf;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.EnabledIf;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@ActiveProfiles("prod")
 class EnvironmentTest {
-
-	@Autowired
-	private Environment environment;
 
 	@Value("${server.port}")
 	private int port;
 
 	@Test
+	@EnabledIf(expression = "#{environment['spring.profiles.active'] == 'test-prod'}", loadContext = true)
 	void verifyProdPort() {
-		assertArrayEquals(new String[]{"prod"}, environment.getActiveProfiles());
 		assertEquals(8090, port);
 	}
 }
