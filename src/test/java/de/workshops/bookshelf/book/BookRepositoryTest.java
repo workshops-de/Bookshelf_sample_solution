d2981a7b098cae59b48500e2d1151101eac5bd4e
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-04-11 19:54:01 +0200

    Solution for task: "Add Liquibase for migrations"

--- src/test/java/de/workshops/bookshelf/book/BookRepositoryTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRepositoryTest.java
@@ -3,6 +3,7 @@ package de.workshops.bookshelf.book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
+@ActiveProfiles("db-test")
 class BookRepositoryTest {
 
     @Autowired
