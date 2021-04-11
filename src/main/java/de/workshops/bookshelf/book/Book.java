d8bc8b2484785839c1eb338b696b15e0b5ee6c35
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-04-11 17:04:36 +0200

    Solution for task: "Create a JPA entity"

--- src/main/java/de/workshops/bookshelf/book/Book.java
+++ src/main/java/de/workshops/bookshelf/book/Book.java
@@ -2,8 +2,18 @@ package de.workshops.bookshelf.book;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String title;
     private String description;
