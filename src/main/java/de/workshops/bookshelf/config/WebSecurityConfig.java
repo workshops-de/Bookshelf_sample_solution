d5887d17784c7387703582e75e2e981e1c889057
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-04-12 12:52:56 +0200

    Solution for task: "Implement RDBMS-based UserDetailsService"

--- src/main/java/de/workshops/bookshelf/config/WebSecurityConfig.java
+++ src/main/java/de/workshops/bookshelf/config/WebSecurityConfig.java
@@ -1,17 +1,27 @@
 package de.workshops.bookshelf.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Collections;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,13 +32,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER").and()
-                .withUser("admin").password("password").roles("USER", "ADMIN");
+        auth
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            String sql = "SELECT * FROM \"user\" WHERE username = ?";
+
+            return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new User(
+                    rs.getString("username"),
+                    rs.getString("password"),
+                    Collections.singletonList(
+                            new SimpleGrantedAuthority(rs.getString("role"))
+                    )
+            ), username);
+        };
     }
 }
