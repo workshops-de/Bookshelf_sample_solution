4c4a78fde8d70a0df41a6258d1f29c85053c18ca
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-09-05 19:09:35 +0200

    Spring Boot upgrade

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerTest.java
@@ -3,7 +3,6 @@ package de.workshops.bookshelf.book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -15,7 +14,6 @@ class BookRestControllerTest {
     private BookRestController bookRestController;
 
     @Test
-    @Sql("data.sql")
     void getAllBooks() {
         assertNotNull(bookRestController.getAllBooks().getBody());
         assertEquals(3, bookRestController.getAllBooks().getBody().size());
