75d7f8655cb527d178c3a85b7085558cb0e7d797
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-11-20 17:51:03 +0100

    Migrating from SpringFox to springdoc-openapi

--- src/main/java/de/workshops/bookshelf/config/SwaggerConfig.java
+++ src/main/java/de/workshops/bookshelf/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("dev")
