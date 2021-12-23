6c5a909c4d39d46fb20194498fbf62c8d2d7871c
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-12-23 19:33:17 +0100

    #7 Upgrade to Spring Boot 2.6.2

--- src/main/java/de/workshops/bookshelf/config/WebSecurityConfig.java
+++ src/main/java/de/workshops/bookshelf/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,6 +19,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests().antMatchers("/").permitAll().and()
                 .authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
     }
 
     @Override
