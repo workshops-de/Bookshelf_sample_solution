776d394c5dc61d8f63bf22b99d57ad51dbbb138e
Björn Wilmsmann <bjoern@wilmsmann.de>
2022-02-22 16:55:50 +0100

    #13

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+DELETE FROM book;
+
+INSERT INTO book (id, isbn, title, author, description) VALUES (1, '978-0201633610', 'Design Patterns', 'Erich Gamma', 'Mit Design Patterns lassen sich wiederkehrende Aufgaben in der objektorientierten Softwareentwicklung effektiv lösen.');
+
+INSERT INTO book (id, isbn, title, author, description) VALUES (2, '978-3826655487', 'Clean Code', 'Robert C. Martin', 'Das einzige praxisnahe Buch, mit dem Sie lernen, guten Code zu schreiben!');
+
+INSERT INTO book (id, isbn, title, author, description) VALUES (3, '978-3836211161', 'Coding for Fun', 'Gottfried Wolmeringer', 'Dieses unterhaltsam geschriebene Buch führt Sie spielerisch durch die spektakuläre Geschichte unserer Blechkollegen.');
