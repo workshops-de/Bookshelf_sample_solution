9a40890c9567b663bf0363f359ab31f426cc7bc6
Björn Wilmsmann <bjoern@wilmsmann.de>
2022-04-26 15:26:05 +0200

    Added default URL for local React to allowed origins

--- src/main/java/de/workshops/bookshelf/book/BookRestController.java
+++ src/main/java/de/workshops/bookshelf/book/BookRestController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping(BookRestController.REQUEST_URL)
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = { "http://localhost:4200", "http://localhost:3000" })
 public class BookRestController {
 
     static final String REQUEST_URL = "/book";
