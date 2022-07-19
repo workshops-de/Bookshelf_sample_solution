845ce81180eccda940e6db57337a41c47ae4e905
Björn Wilmsmann <bjoern@wilmsmann.de>
2022-07-19 22:00:42 +0200

    #35

--- src/main/java/de/workshops/bookshelf/config/WebSecurityConfig.java
+++ src/main/java/de/workshops/bookshelf/config/WebSecurityConfig.java
@@ -3,84 +3,32 @@ package de.workshops.bookshelf.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-=======
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
->>>>>>> Add_more_credentials
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-<<<<<<< HEAD
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-=======
-import org.springframework.security.web.SecurityFilterChain;
->>>>>>> 7061165f9027e5aea2c956df6f8cccedd5b029e1
 
 import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-<<<<<<< HEAD
-<<<<<<< HEAD
-@EnableWebSecurity
-=======
->>>>>>> 7061165f9027e5aea2c956df6f8cccedd5b029e1
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
     private final JdbcTemplate jdbcTemplate;
 
-<<<<<<< HEAD
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
-=======
-public class WebSecurityConfig {
-
-=======
->>>>>>> 7061165f9027e5aea2c956df6f8cccedd5b029e1
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .httpBasic(withDefaults())
                 .formLogin(withDefaults())
                 .build();
-<<<<<<< HEAD
->>>>>>> Add_more_credentials
-    }
-
-    @Bean
-    WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().antMatchers("/h2-console/**");
-    }
-
-<<<<<<< HEAD
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userDetailsService())
-                .passwordEncoder(passwordEncoder());
-=======
-    @Bean
-    UserDetailsService userDetailsService() {
-        var user = User.builder().username("user").password("password").roles("USER").build();
-        var admin = User.builder().username("admin").password("admin").roles("USER", "ADMIN").build();
-        return new InMemoryUserDetailsManager(user, admin);
->>>>>>> Add_more_credentials
-=======
->>>>>>> 7061165f9027e5aea2c956df6f8cccedd5b029e1
     }
 
     @Bean
