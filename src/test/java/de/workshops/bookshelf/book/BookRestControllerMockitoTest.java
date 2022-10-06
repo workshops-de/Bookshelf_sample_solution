c3e61226e4a30eca8ba0925d18fb0bcf34e67d93
Björn Wilmsmann <bjoern@wilmsmann.de>
2022-10-06 14:26:48 +0200

    More Mockito test variations

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerMockitoTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerMockitoTest.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(BookRestController.class)
 class BookRestControllerMockitoTest {
@@ -22,8 +22,15 @@ class BookRestControllerMockitoTest {
 
     @Test
     void getAllBooks() {
+        Mockito.when(bookService.getBooks()).thenReturn(Collections.singletonList(new Book()));
+
+        assertNotNull(bookRestController.getAllBooks().getBody());
+        assertEquals(HttpStatus.OK, bookRestController.getAllBooks().getStatusCode());
+        assertEquals(1, bookRestController.getAllBooks().getBody().size());
+
         Mockito.when(bookService.getBooks()).thenReturn(Collections.emptyList());
 
+        assertNull(bookRestController.getAllBooks().getBody());
         assertEquals(HttpStatus.NOT_FOUND, bookRestController.getAllBooks().getStatusCode());
     }
 }
