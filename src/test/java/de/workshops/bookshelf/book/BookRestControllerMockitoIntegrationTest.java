a52b3b6fbdddfc5bb4296e3ecdd73b24b97f2fc6
Björn Wilmsmann <bjoern@bjoernkw.com>
2022-10-24 15:59:36 +0200

    Updating sample solution branches #45 #40 #39 #47 #1

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerMockitoIntegrationTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerMockitoIntegrationTest.java
@@ -3,15 +3,24 @@ package de.workshops.bookshelf.book;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest(BookRestController.class)
+@WebMvcTest(
+        controllers = BookRestController.class,
+        includeFilters = @ComponentScan.Filter(
+                type = FilterType.ANNOTATION,
+                classes = ConfigurationProperties.class
+        )
+)
 @AutoConfigureMockMvc
 class BookRestControllerMockitoIntegrationTest {
 
