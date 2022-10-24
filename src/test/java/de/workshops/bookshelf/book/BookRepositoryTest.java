dd0827f0a1868147e3883f592167f743e30c5e46
Björn Wilmsmann <bjoern@bjoernkw.com>
2022-10-24 17:37:19 +0200

    Merge branch 'Use_a_persistent_H2_database' into Create_and_test_a_BookRepository_based_on_JDBCTemplate
    # Conflicts:
    #	src/main/java/de/workshops/bookshelf/book/Book.java
    #	src/test/java/de/workshops/bookshelf/book/BookRestControllerMockitoReflectionTestUtilsTest.java

--- src/test/java/de/workshops/bookshelf/book/BookRepositoryTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRepositoryTest.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
