94a59901baba379557326de16d3273ad338c9436
Birgit Kratz <mail@birgitkratz.de>
2022-11-10 23:19:38 +0100

    add validation example to BookRestController

--- src/main/java/de/workshops/bookshelf/book/BookRestController.java
+++ src/main/java/de/workshops/bookshelf/book/BookRestController.java
@@ -1,29 +1,46 @@
 package de.workshops.bookshelf.book;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/book")
+@Validated
 public class BookRestController {
 
-    @Autowired
-    private ObjectMapper mapper;
+    private final ObjectMapper mapper;
+    private final ResourceLoader resourceLoader;
 
     private List<Book> books;
 
+    public BookRestController(ObjectMapper mapper, ResourceLoader resourceLoader) {
+        this.mapper = mapper;
+        this.resourceLoader = resourceLoader;
+    }
+
     @PostConstruct
     public void init() throws IOException {
-        this.books = Arrays.asList(mapper.readValue(new File("target/classes/books.json"), Book[].class));
+        final var resource = resourceLoader.getResource("classpath:books.json");
+        books = mapper.readValue(resource.getInputStream(), new TypeReference<>() {});
     }
     
     @GetMapping
@@ -37,15 +54,15 @@ public class BookRestController {
     }
 
     @GetMapping(params = "author")
-    public Book searchBookByAuthor(@RequestParam String author) throws BookException {
+    public Book searchBookByAuthor(@RequestParam @NotBlank @Size(min = 3) String author) throws BookException {
         return this.books.stream().filter(book -> hasAuthor(book, author)).findFirst().orElseThrow(BookException::new);
     }
 
     @PostMapping("/search")
-    public List<Book> searchBooks(@RequestBody BookSearchRequest request) {
+    public List<Book> searchBooks(@RequestBody @Valid BookSearchRequest request) {
         return this.books.stream()
-                .filter(book -> hasAuthor(book, request.getAuthor()))
-                .filter(book -> hasIsbn(book, request.getIsbn()))
+                .filter(book -> hasAuthor(book, request.author()))
+                .filter(book -> hasIsbn(book, request.isbn()))
                 .toList();
     }
 
