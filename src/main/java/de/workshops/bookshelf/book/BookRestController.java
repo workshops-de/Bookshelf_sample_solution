fbd0203de9fada037144d049966309850775e7d6
Birgit Kratz <mail@birgitkratz.de>
2022-11-15 23:59:01 +0100

    secure book creation for role ADMIN

--- src/main/java/de/workshops/bookshelf/book/BookRestController.java
+++ src/main/java/de/workshops/bookshelf/book/BookRestController.java
@@ -3,6 +3,7 @@ package de.workshops.bookshelf.book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -47,6 +48,7 @@ public class BookRestController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public Book createBook(@RequestBody Book book) {
         return bookService.createBook(book);
     }
