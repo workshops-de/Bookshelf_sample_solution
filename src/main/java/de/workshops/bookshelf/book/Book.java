e4a64d17b358518916c309a529a289d0842ba1ad
Björn Wilmsmann <bjoern@bjoernkw.com>
2022-11-29 14:53:30 +0100

    #52

--- src/main/java/de/workshops/bookshelf/book/Book.java
+++ src/main/java/de/workshops/bookshelf/book/Book.java
@@ -20,7 +20,7 @@ import java.util.Objects;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
