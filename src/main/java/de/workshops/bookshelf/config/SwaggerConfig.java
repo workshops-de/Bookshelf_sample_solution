cab0a9517b6e84690a997fb051273b409c1bb3d9
Björn Wilmsmann <bjoern@bjoernkw.com>
2022-12-12 11:40:04 +0100

    Springdoc update for Spring Boot 3

--- src/main/java/de/workshops/bookshelf/config/SwaggerConfig.java
+++ src/main/java/de/workshops/bookshelf/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package de.workshops.bookshelf.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,6 +11,13 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("dev")
 public class SwaggerConfig {
+
+    @Bean
+    GroupedOpenApi appOpenApi() {
+        String[] packagesToScan = { "de.workshops.bookshelf" };
+
+        return GroupedOpenApi.builder().group("app").packagesToScan(packagesToScan).build();
+    }
 
     @Bean
     public OpenAPI api() {
