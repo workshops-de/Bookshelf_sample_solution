fe00685d405247aa2bbf5e2896c5af65b4658ca8
Björn Wilmsmann <bjoern@bjoernkw.com>
2022-12-19 11:50:59 +0100

    #48 Spring Boot upgrade

--- src/main/java/de/workshops/bookshelf/BookRestController.java
+++ src/main/java/de/workshops/bookshelf/BookRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.List;
 
