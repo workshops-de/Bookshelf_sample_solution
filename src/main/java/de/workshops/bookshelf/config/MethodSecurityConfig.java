19b87795cc88b4230721574861b8cf70ac8ea7e0
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-03-27 17:24:48 +0200

    Method security configuration

--- src/main/java/de/workshops/bookshelf/config/MethodSecurityConfig.java
+++ src/main/java/de/workshops/bookshelf/config/MethodSecurityConfig.java
@@ -1,7 +1,9 @@
 package de.workshops.bookshelf.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@Configuration
 @EnableMethodSecurity
 public class MethodSecurityConfig {
 }
