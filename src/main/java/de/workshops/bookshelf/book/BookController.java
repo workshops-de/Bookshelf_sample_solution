edf04fd0b13a6aeb59f980e02246f59a9743f2ad
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-11-26 23:01:54 +0100

    Spring Boot update

--- src/main/java/de/workshops/bookshelf/book/BookController.java
+++ src/main/java/de/workshops/bookshelf/book/BookController.java
@@ -9,21 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(BookController.REQUEST_URL)
 @RequiredArgsConstructor
-public class BookController {
+class BookController {
 
     static final String REQUEST_URL = "/";
 
     private final BookService bookService;
 
     @GetMapping
-    public String getAllBooks(Model model) {
+    String getAllBooks(Model model) {
         model.addAttribute("books", bookService.getAllBooks());
 
         return "books";
     }
 
     @GetMapping("/success")
-    public String redirectToSuccessUrl(Model model) {
+    String redirectToSuccessUrl(Model model) {
         return getAllBooks(model);
     }
 }
--- src/main/java/de/workshops/bookshelf/book/BookRestController.java
+++ src/main/java/de/workshops/bookshelf/book/BookRestController.java
@@ -3,31 +3,39 @@ package de.workshops.bookshelf.book;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import java.net.URI;
+import java.time.Instant;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
-import java.time.Instant;
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(BookRestController.REQUEST_URL)
 @RequiredArgsConstructor
 @Validated
 @CrossOrigin(origins = { "http://localhost:4200", "http://localhost:3000" })
-public class BookRestController {
+class BookRestController {
 
     static final String REQUEST_URL = "/book";
 
     private final BookService bookService;
 
     @GetMapping
-    public ResponseEntity<List<Book>> getAllBooks() {
+    ResponseEntity<List<Book>> getAllBooks() {
         List<Book> result = bookService.getAllBooks();
         if (result.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -37,35 +45,35 @@ public class BookRestController {
     }
 
     @GetMapping("/{isbn}")
-    public Book getBookByIsbn(@PathVariable String isbn) throws BookException {
+    Book getBookByIsbn(@PathVariable String isbn) throws BookException {
         return bookService.searchBookByIsbn(isbn);
     }
 
     @GetMapping(params = "author")
-    public Book getBookByAuthor(@RequestParam @NotBlank @Size(min = 3) String author) throws BookException {
+    Book getBookByAuthor(@RequestParam @NotBlank @Size(min = 3) String author) throws BookException {
         return bookService.searchBookByAuthor(author);
     }
 
     @PostMapping("/search")
-    public List<Book> searchBooks(@RequestBody @Valid BookSearchRequest request) {
+    List<Book> searchBooks(@RequestBody @Valid BookSearchRequest request) {
         return bookService.searchBooks(request);
     }
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public Book createBook(@RequestBody Book book) {
+    Book createBook(@RequestBody Book book) {
         return bookService.createBook(book);
     }
 
     @DeleteMapping("/{isbn}")
-    public ResponseEntity<String> deleteBook(@PathVariable String isbn) throws BookException {
+    ResponseEntity<String> deleteBook(@PathVariable String isbn) throws BookException {
         bookService.deleteBook(bookService.searchBookByIsbn(isbn));
 
         return ResponseEntity.ok("OK");
     }
 
     @ExceptionHandler(BookException.class)
-    public ProblemDetail error(BookException ex) {
+    ProblemDetail error(BookException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
         problemDetail.setTitle("Book Not Found");
         problemDetail.setType(URI.create("http://localhost:8080/book_exception.html"));
