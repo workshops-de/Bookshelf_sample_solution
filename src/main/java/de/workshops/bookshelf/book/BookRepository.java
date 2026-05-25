324707cfac596150dbf44421b14ec5bcc874f566
Björn Wilmsmann <bjoern@bjoernkw.com>
2026-05-25 23:33:13 +0200

    Spring Boot update

--- src/main/java/de/workshops/bookshelf/book/BookRepository.java
+++ src/main/java/de/workshops/bookshelf/book/BookRepository.java
@@ -1,41 +1,10 @@
 package de.workshops.bookshelf.book;
 
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RequiredArgsConstructor
-class BookRepository {
+interface BookRepository extends ListCrudRepository<Book, Long> {
 
-    private final JdbcTemplate jdbcTemplate;
-
-    public List<Book> findAllBooks() {
-        String sql = "SELECT * FROM book";
-
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Book.class));
-    }
-
-    public void createBook(Book book) {
-        String sql = "INSERT INTO book (title, description, author, isbn) VALUES (?, ?, ?, ?)";
-
-        jdbcTemplate.update(
-                sql,
-                book.getTitle(),
-                book.getDescription(),
-                book.getAuthor(),
-                book.getIsbn()
-        );
-    }
-
-    public void deleteBook(Book book) {
-        String sql = "DELETE FROM book WHERE isbn = ?";
-
-        jdbcTemplate.update(
-                sql,
-                book.getIsbn()
-        );
-    }
+    Book findByIsbn(String isbn);
 }
