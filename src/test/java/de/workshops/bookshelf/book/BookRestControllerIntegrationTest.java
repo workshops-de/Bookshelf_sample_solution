afaeda4df5f6c9892c41348a4206abbb7a4479ba
Björn Wilmsmann <bjoern@bjoernkw.com>
2026-05-25 18:29:23 +0200

    Spring Boot update

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerIntegrationTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerIntegrationTest.java
@@ -7,9 +7,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.restassured.RestAssured;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
