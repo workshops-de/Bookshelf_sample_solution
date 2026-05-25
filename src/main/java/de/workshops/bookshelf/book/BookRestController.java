0e2187a33620c17e7025dfd3fb3429761f8b6839
Björn Wilmsmann <bjoern@bjoernkw.com>
2026-05-26 00:03:06 +0200

    Spring Boot update

--- src/main/java/de/workshops/bookshelf/book/BookRestController.java
+++ src/main/java/de/workshops/bookshelf/book/BookRestController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/book")
 @RequiredArgsConstructor
