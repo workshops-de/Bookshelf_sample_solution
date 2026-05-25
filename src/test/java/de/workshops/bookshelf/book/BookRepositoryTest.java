e8f44f3737441d5b5c4c79a7be6ee02eb259d8e6
Björn Wilmsmann <bjoern@bjoernkw.com>
2026-05-25 23:37:25 +0200

    Spring Boot update

--- src/test/java/de/workshops/bookshelf/book/BookRepositoryTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRepositoryTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
